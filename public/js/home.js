a87277629d14d6eddd504a89373c3ed19c4f2089
mohan-18 <mohangorai4@gmail.com>
2021-04-15 09:22:12 +0530

    CSS Added

--- public/js/home.js
+++ public/js/home.js
@@ -10,15 +10,19 @@ socket.on('message',function(message){
 submit.addEventListener("click",(e)=>{
     e.preventDefault()
     
+    //div.scrollHeight - div.clientHeight;
     if(input.value){
         console.log(input.value);
         socket.emit('chat_msg',input.value);
+        input.value="";
     }
 })
 
 function insert_msg(msg){
     div_m = document.createElement('div')
-    div_m.classList.add('msg');
-    div_m.innerHTML=msg;
+    div_m.classList.add('msg','left');
+    div_m.innerHTML="<p>"+msg +"</p>";
     msg_div.appendChild(div_m);
+    msg_div.scrollTop = msg_div.scrollHeight;
+
 }
